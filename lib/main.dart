44d6437e6c44d77660569d2f5705804536d99506
Alex Cocan <al_razvan@yahoo.com>
2024-02-29 11:32:01 +0100

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
-void main() => runApp(XylophoneApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(XylophoneApp());
+}
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
+  @override
+  _XylophoneAppState createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  void playSound(int soundNumber) async {
+    final AudioPlayer player = AudioPlayer();
+    await player.play(AssetSource('note$soundNumber.wav'));
+  }
+
+  Expanded buildKey({required Color color, required int soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          padding: EdgeInsets
+              .zero, // Removes padding to fill the button in the Expanded widget
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+              // Makes the button's corners not rounded
+              borderRadius: BorderRadius.zero),
+        ),
+        onPressed: () {
+          playSound(soundNumber);
+        },
+        child: Text(''), // You can add text or leave it empty
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment
+                .stretch, // Makes the buttons stretch to fill the screen width
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.purple, soundNumber: 7),
+            ],
+          ),
         ),
       ),
     );
